e76d9d360893609f7a7b916b0217867ad6605fcf
Dmitry Romashov <framebassman@gmail.com>
2023-07-19 06:44:07 +0200

    Fix path for logo

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -2,7 +2,7 @@
   <img
     class="VuetifyLogo"
     alt="Vuetify Logo"
-    src="/vuetify-logo.svg"
+    src="https://github.com/FrameBassman/squadom/raw/master/static/vuetify-logo.svg"
   >
 </template>
 
